fa360a3389c2fe7db96723aa97c6d5cf7f67d70e
elifpehlivan <elifpehlivan1@gmail.com>
2024-01-21 01:21:03 +0300

    deneme 2-3

--- src/git_review01/HelloWorld.java
+++ src/git_review01/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is Hello World Class");
 
-        System.out.println("deneme1-2");
+        System.out.println("deneme1-2-3");
 
     }
 }
